b43b05a8ad6c08ba603c2acc83a2d384d31f3815
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-13 20:00:18 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,15 +10,20 @@ require "pry"
 
 
 def create_project_hash
-  # html = File.read('fixtures/kickstarter.html')
-  # kickstarter = Nokogiri::HTML(html)
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
 
   projects = {}
 
  # Iterate through the projects
-  kickstarter.css("li.project.grid_4").each do |project|
+  kickstarter.css("li.project.grid_4").each do |project| #we're converting the title into a symbol using the to_sym method.
     title = project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {} #we're converting the title into a symbol using the to_sym method.
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }
  end
 
    # return the projects hash
